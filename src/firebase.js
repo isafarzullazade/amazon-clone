17d61df2dcf7dcb62e83c0a44be9283b2c3b28cb
unknown <isaferzulla@gmail.com>
2021-02-25 17:12:47 +0400

    navbar is done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDIV3pq9CQkVv5kDRtB8IeSKKnyOvKs_kA",
+  authDomain: "challange-a44a4.firebaseapp.com",
+  projectId: "challange-a44a4",
+  storageBucket: "challange-a44a4.appspot.com",
+  messagingSenderId: "363765162940",
+  appId: "1:363765162940:web:576c8ce9b05e58be4801b9",
+  measurementId: "G-3JRHM22K6L",
+};
